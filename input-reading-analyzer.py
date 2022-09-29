ded2792a3cd1cea90cbbec3ec942181a439e2650
magaika <105580481+magaika@users.noreply.github.com>
2022-09-29 16:18:27 +0300

    added min and max
    at the end the program prints out both the maximum and minimum of the numbers

--- input-reading-analyzer.py
+++ input-reading-analyzer.py
@@ -1,6 +1,8 @@
 total = 0
 count = 0
 average = 0
+largest = None
+smallest = None
 while True:
     try:
         line = input("Enter a number: ")
@@ -9,6 +11,10 @@ while True:
         total = total + int(line)
         count = count + 1 
         average = total / count
+        if largest == None or line > largest:
+            largest = line
+        if smallest == None or line < smallest:
+            smallest = line
     except:
         print("Invalid input")
-print(total, count, average)
+print(total, count, average, largest, smallest)
