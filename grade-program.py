f9f9e58a729c30e463d99e8358e4298641e0a661
magaika <105580481+magaika@users.noreply.github.com>
2022-09-29 11:33:43 +0300

    added function computegrade
    Rewrite the grade program using a function called computegrade that takes a score as its parameter and returns a grade as a string.

--- grade-program.py
+++ grade-program.py
@@ -1,17 +1,20 @@
-try:
-    score = float(input("Enter score: "))
-    if 1.0 > score > 0.0:
-        if score >= 0.9:
+def computegrade(score0):
+    if 1.0 > score0 > 0.0:
+        if score0 >= 0.9:
             print("A")
-        elif score >= 0.8:
+        elif score0 >= 0.8:
             print("B")
-        elif score >= 0.7:
+        elif score0 >= 0.7:
             print("C")
-        elif score >= 0.6:
+        elif score0 >= 0.6:
             print("D")
-        elif 0.0 < score < 0.6:
+        elif 0.0 < score0 < 0.6:
             print("F")
     else: 
         print("Bad score")
+
+try:
+    score = float(input("Enter score: "))
+    computegrade(score)
 except:
     print("Bad score")
