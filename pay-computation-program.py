684b75300850d5f72e2c9a859b3c6bd9422f2600
magaika <105580481+magaika@users.noreply.github.com>
2022-09-29 11:25:53 +0300

    Update using try and except
    Rewrite pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program.

--- pay-computation-program.py
+++ pay-computation-program.py
@@ -1,7 +1,10 @@
-hours = int(input("Enter Hours: "))
-rate = int(input("Enter Rate: "))
-if hours > 40:
-    pay = (hours - 40) * (rate * 1.5) + (40 * rate)
-else:
-    pay = hours * rate
-print("Pay: ", pay)
+try:
+    hours = int(input("Enter Hours: "))
+    rate = int(input("Enter Rate: "))
+    if hours > 40:
+        pay = (hours - 40) * (rate * 1.5) + (40 * rate)
+    else:
+        pay = hours * rate
+    print("Pay: ", pay)
+except:
+    print("Error, please enter numeric input")
