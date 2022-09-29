0fd1d5004afa61726ba2a5386bb06e124195af7c
magaika <105580481+magaika@users.noreply.github.com>
2022-09-29 11:27:35 +0300

    create a function computepay
    Rewrite pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters (hours and rate).

--- pay-computation-program.py
+++ pay-computation-program.py
@@ -1,10 +1,12 @@
-try:
-    hours = int(input("Enter Hours: "))
-    rate = int(input("Enter Rate: "))
+def computepay(hours, rate):
     if hours > 40:
         pay = (hours - 40) * (rate * 1.5) + (40 * rate)
     else:
         pay = hours * rate
     print("Pay: ", pay)
+try:
+    h = int(input("Enter Hours: "))
+    r = int(input("Enter Rate: "))
+    computepay(h, r)
 except:
     print("Error, please enter numeric input")
